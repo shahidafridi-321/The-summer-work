4b0106b60f80e88c68b4ef16fcce03dda45582fc
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-07-18 16:12:06 +0500

    ES6 module

---
+++ export and import/example1/my-module.js
@@ -0,0 +1,19 @@
+// module "my-module.js"
+function cube(x) {
+  return x * x * x;
+}
+
+const foo = Math.PI + Math.SQRT2;
+
+const graph = {
+  options: {
+    color: "white",
+    thickness: "2px",
+  },
+  draw() {device-width
+    console.log("From graph draw function");
+  },
+};
+
+export { cube, foo, graph };
+console.log(graph.options);
