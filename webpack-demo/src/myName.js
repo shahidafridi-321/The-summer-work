f059c2a31c6a46ec2e354fc035ad987037f0b84e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-07-19 13:24:57 +0500

    html-webpack-plugin

--- webpack-demo/src/myName.js
+++
@@ -1,5 +0,0 @@
-const myName = (name) => 'My name is ' + name;
-
-// export {myName};  Named exports
-
-export default myName // dafault exports
---
+++ webpack/html-webpack-plugin/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode:'development',
+  entry:'./src/index.js',
+  output:{
+    filename:'bundle.js',
+    path:path.resolve(__dirname,'dist'),
+  },
+  devServer: {
+    contentBase:path.join(__dirname,'dist'),
+    port:3000,
+    open:true,
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./src/index.html',
+      filename:'index.html',
+    }),
+  ],
+
+};
