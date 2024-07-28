d9dc56efe5ef7459308129822cf886da3aa434de
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-07-28 17:18:56 +0500

    react/part2/src/App.jsx

--- react/part2/src/App.jsx
+++ react/part2/src/App.jsx
@@ -1,8 +1,22 @@
+import  './index.css';
+import img from './footer-bg.png'
+const Profile = () => {
+  return (
+    <div className="container">
+      <img src={img} width="40px" height="40px" alt="" />
+      <h2>shahid</h2>
+      <button>Follow</button>
+    </div>
+
+  )
+};
+
 const App = () => {
   return (
     <div>
-      <h1>Shahid Afridi</h1>
-      <h1>Shahid Afridi</h1>
+    <Profile/>
+
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit quis recusandae cumque eveniet aspernatur ducimus dicta repellat facilis, tempora odio pariatur consequatur fugit, quos reprehenderit! Consequatur odit voluptates non suscipit!</p>
     </div>
   )
 }
