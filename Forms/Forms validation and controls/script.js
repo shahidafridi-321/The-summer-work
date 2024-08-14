39699ab4e33ca568e32c25b4fe2f36fb8b5a9662
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-14 12:42:32 +0500

    Form validation using constriant API

--- Forms/Forms validation and controls/script.js
+++ Forms/Forms validation and controls/script.js
@@ -4,6 +4,9 @@ let emailErrorMessage = document.getElementById("email-error");
 let zipCode = document.getElementById("zip-code");
 let zipCodeErrorMessage = document.querySelector("span.zip-code-error");
 
+let country = document.querySelector("#country");
+let countryErrorMessage = document.querySelector(".country-error");
+
 email.addEventListener("input", () => {
   if (email.validity.valid) {
     emailErrorMessage.textContent = "correct";
@@ -14,8 +17,26 @@ email.addEventListener("input", () => {
   }
 });
 
+country.addEventListener("change", () => {
+  if (!country.value) {
+    countryErrorMessage.textContent = "please select country";
+    countryErrorMessage.classList.add("error");
+    countryErrorMessage.classList.remove("correct");
+  } else {
+    countryErrorMessage.textContent = "correct";
+    countryErrorMessage.classList.add("correct");
+    countryErrorMessage.classList.remove("error");
+    setZipCodePattern();
+    showError(zipCode, zipCodeErrorMessage);
+  }
+});
+
 zipCode.addEventListener("input", () => {
-  if (zipCode.validity.valid) {
+  if (!country.value) {
+    zipCodeErrorMessage.textContent = "please select country first";
+    zipCodeErrorMessage.classList.add("error");
+    zipCodeErrorMessage.classList.remove("correct");
+  } else if (zipCode.validity.valid) {
     zipCodeErrorMessage.textContent = "correct";
     zipCodeErrorMessage.classList.add("correct");
     zipCodeErrorMessage.classList.remove("error");
@@ -33,5 +54,28 @@ function showError(input, message) {
     message.textContent = `please write ${input.id} in correct format`;
     message.classList.remove("correct");
     message.classList.add("error");
+  } else if (input.validity.patternMismatch) {
+    if (input === zipCode) {
+      message.textContent = "Invalid zip code format for " + country.value;
+      message.classList.remove("correct");
+      message.classList.add("error");
+    } else {
+    }
+  }
+}
+
+function setZipCodePattern() {
+  zipCode.removeAttribute("pattern");
+  let zipCodePattern = {
+    england: "^[A-Z]{1,2}\\d[A-Z\\d]? \\d[A-Z]{2}$", // Example: "W1A 1AA"
+    australia: "^\\d{4}$",
+    finland: "^\\d{5}$",
+    germany: "^\\d{5}$",
+  };
+  let selectedPattern = zipCodePattern[country.value];
+  if (selectedPattern) {
+    zipCode.setAttribute("pattern", selectedPattern);
+  } else {
+    zipCode.removeAttribute("pattern");
   }
 }
