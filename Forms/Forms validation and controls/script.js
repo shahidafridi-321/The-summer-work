d0fe4f14764e48ca7a88fde6b48f4ffe624312d1
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-14 15:24:47 +0500

    passwords validation

--- Forms/Forms validation and controls/script.js
+++ Forms/Forms validation and controls/script.js
@@ -7,6 +7,14 @@ let zipCodeErrorMessage = document.querySelector("span.zip-code-error");
 let country = document.querySelector("#country");
 let countryErrorMessage = document.querySelector(".country-error");
 
+let password = document.getElementById("password");
+let passwordErrorMessage = document.querySelector(".password-error");
+
+let confirmPassword = document.getElementById("confirm-password");
+let confirmPasswordErrorMessage = document.querySelector(
+  ".confirm-password-error"
+);
+
 email.addEventListener("input", () => {
   if (email.validity.valid) {
     emailErrorMessage.textContent = "correct";
@@ -45,23 +53,52 @@ zipCode.addEventListener("input", () => {
   }
 });
 
+password.addEventListener("input", () => {
+  if (confirmPassword.value !== password.value) {
+    checksPasswords();
+  }
+  if (password.validity.valid) {
+    passwordErrorMessage.textContent = "correct";
+    passwordErrorMessage.classList.add("correct");
+    passwordErrorMessage.classList.remove("error");
+  } else {
+    showError(password, passwordErrorMessage);
+    showError(confirmPassword,confirmPasswordErrorMessage);
+  }
+});
+
+confirmPassword.addEventListener("input", () => {
+  if (!password.value) {
+    confirmPasswordErrorMessage.textContent = "Please choose a password first";
+    confirmPasswordErrorMessage.classList.add("error");
+    confirmPasswordErrorMessage.classList.remove("correct");
+  } else if (confirmPassword.value !== password.value) {
+    checksPasswords();
+  } else if (confirmPassword.validity.valid) {
+    confirmPasswordErrorMessage.textContent = "correct";
+    confirmPasswordErrorMessage.classList.add("correct");
+    confirmPasswordErrorMessage.classList.remove("error");
+  } else {
+    showError(confirmPassword, confirmPasswordErrorMessage);
+  }
+});
+
 function showError(input, message) {
   if (input.validity.valueMissing) {
     message.textContent = `${input.id} is required`;
-    message.classList.remove("correct");
-    message.classList.add("error");
   } else if (input.validity.typeMismatch) {
     message.textContent = `please write ${input.id} in correct format`;
     message.classList.remove("correct");
     message.classList.add("error");
-  } else if (input.validity.patternMismatch) {
-    if (input === zipCode) {
-      message.textContent = "Invalid zip code format for " + country.value;
-      message.classList.remove("correct");
-      message.classList.add("error");
-    } else {
-    }
+  } else if (input.validity.patternMismatch && input === zipCode) {
+    message.textContent = "Invalid zip code format for " + country.value;
+  } else if (input.validity.patternMismatch && input === password) {
+    message.textContent = `Password should include`;
+  } else if (input.validity.tooShort) {
+    message.textContent = `The ${input.id} must be atleast ${input.minLength} charaters long`;
   }
+  message.classList.remove("correct");
+  message.classList.add("error");
 }
 
 function setZipCodePattern() {
@@ -77,5 +114,13 @@ function setZipCodePattern() {
     zipCode.setAttribute("pattern", selectedPattern);
   } else {
     zipCode.removeAttribute("pattern");
+  }
+}
+
+function checksPasswords() {
+  if (password.value) {
+    confirmPasswordErrorMessage.textContent = "Password did not match";
+    confirmPasswordErrorMessage.classList.add("error");
+    confirmPasswordErrorMessage.classList.remove("correct");
   }
 }
