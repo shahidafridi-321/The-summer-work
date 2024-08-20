63a83845fd3e9d0b1a3446debcf739e51e18fcc9
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-20 15:06:15 +0500

    promises

---
+++ JavaScript/promises/ex8.js
@@ -0,0 +1,15 @@
+const fetchPromise = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+
+fetchPromise
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => console.log(data[0].name))
+  .catch((error)=>{
+    console.log(`Couldn't get products: ${error}`);
+  });
