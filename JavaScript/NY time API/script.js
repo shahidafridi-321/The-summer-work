0fa3d75734bcd9d5f9afd2af5464d83a96c2cff9
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-25 21:33:45 +0500

    nyt api

---
+++ JavaScript/NY time API/script.js
@@ -0,0 +1,103 @@
+// Defining a baseURL and key to as part of the request URL
+
+const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const key = "OSjs8QawB0nv8abJwjrFwELrOUkzOstK";
+
+// Grab references to all the DOM elements you'll need to manipulate
+const searchTerm = document.querySelector(".search");
+const startDate = document.querySelector(".start-date");
+const endDate = document.querySelector(".end-date");
+const searchForm = document.querySelector("form");
+const nextBtn = document.querySelector(".next");
+const previousBtn = document.querySelector(".prev");
+const section = document.querySelector("section");
+const nav = document.querySelector("nav");
+
+// Hide the "Previous"/"Next" navigation to begin with, as we don't need it immediately
+nav.style.display = "none";
+
+// define the initial page number and status of the navigation being displayed
+let pageNumber = 0;
+
+// Event listeners to control the functionality
+
+searchForm.addEventListener("submit", submitSearch);
+
+function submitSearch() {
+	pageNumber = 0;
+	fetchApi(e);
+}
+
+function fetchApi(e) {
+	e.preventDefault();
+
+	let url = `${baseURL}?api-key=${key}&page=${pageNumber}&q=${searchTerm.value}&fq=document_type:("article")`;
+
+	if (startDate.value !== "") {
+		url = `${url}&begin_date=${startDate.value}`;
+	}
+
+	if (endDate.value !== "") {
+		url = `${url}&end_date=${endDate.value}`;
+	}
+}
+
+fetch(url)
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error("HTTP Error :", response.status);
+		}
+		return response.json();
+	})
+	.then((json) => displayResults(json))
+	.catch((error) => console.log("Could not fetch:", error));
+
+function displayResults(json) {
+	while (section.firstChild) {
+		section.removeChild(section.firstChild);
+	}
+
+	const articles = json.response.docs;
+
+	nav.style.display = articles.length === 10 ? "block" : "none";
+
+	if (articles.length === 0) {
+		const para = document.createElement("p");
+		para.textContent = "No results returned.";
+		section.appendChild(para);
+	} else {
+		for (const current of articles) {
+			const article = document.createElement("article");
+			const heading = document.createElement("h2");
+			const link = document.createElement("a");
+			const img = document.createElement("img");
+			const para1 = document.createElement("p");
+			const keywordPara = document.createElement("p");
+			keywordPara.classList.add("keywords");
+
+			console.log(current);
+
+			link.href = current.web_url;
+			link.textContent = current.headline.main;
+			para1.textContent = current.snippet;
+			keywordPara.textContent = "Keywords: ";
+			for (const keyword of current.keywords) {
+				const span = document.createElement("span");
+				span.textContent = `${keyword.value} `;
+				keywordPara.appendChild(span);
+			}
+
+			if (current.multimedia.length > 0) {
+				img.src = `http://www.nytimes.com/${current.multimedia[0].url}`;
+				img.alt = current.headline.main;
+			}
+
+			article.appendChild(heading);
+			heading.appendChild(link);
+			article.appendChild(img);
+			article.appendChild(para1);
+			article.appendChild(keywordPara);
+			section.appendChild(article);
+		}
+	}
+}
