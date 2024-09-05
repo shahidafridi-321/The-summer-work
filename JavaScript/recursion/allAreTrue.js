fc36e179c897dd1e3c4cb20f5eb2960286a82b65
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-05 20:30:26 +0500

    all less then seven

---
+++ JavaScript/recursion/allAreTrue.js
@@ -0,0 +1,14 @@
+function all(array, callBack) {
+	if (array.length === 0) {
+		return true;
+	}
+	if (!callBack(array[0])) {
+		return false;
+	}
+
+	return all(array.slice(1), callBack);
+}
+function allLessthanSeven(num) {
+	return num < 7;
+}
+console.log(all([1, 2, 9]),allLessthanSeven);
