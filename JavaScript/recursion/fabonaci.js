3c844104cd4554cebe7d918b799c8686e79e5747
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-05 11:43:12 +0500

    display single link list item

--- JavaScript/recursion/fabonaci.js
+++ JavaScript/recursion/fabonaci.js
@@ -1,31 +1,22 @@
 // loop version of fabonaci series
 function fabLoop(num) {
+	if (num <= 1) {
+		return num;
+	}
 	let prev = 0;
 	let next = 1;
-	let sum = 0;
 	for (let i = 2; i <= num; i++) {
-		sum = prev + next;
+		let sum = prev + next;
 		prev = next;
 		next = sum;
 	}
-	return sum;
+	return next;
 }
 /* console.log(fabLoop(4));
 console.log(fabLoop(7));
 console.log(fabLoop(77));
  */
 // recursion version of fabonaci series
-/* 
-number = 3;
-return num-1 + fab(num-1)
-
-(3-1) + fab(3-1) = 2+6 return 10;
-2 + 1
-() + fab(2) = 3+3 return 6;
-2 + 1 return 3
-
-
-*/
 
 function fabRecursion(num) {
 	if (num <= 1) {
---
+++ JavaScript/recursion/outputAsingleLinkList.js
@@ -0,0 +1,54 @@
+let list = {
+	value: 1,
+	next: {
+		value: 2,
+		next: {
+			value: 3,
+			next: {
+				value: 4,
+				next: null,
+			},
+		},
+	},
+};
+/* 
+function printLinkList(list) {
+	console.log(list);
+
+	for (let subList of Object.values(list)) {
+		if (typeof subList == "object") {
+			console.log(subList);
+			for (let subsubList of Object.values(subList)) {
+				if (typeof subsubList == "object") {
+					console.log(subsubList);
+					for (let subsubsublist of Object.values(subsubList)) {
+						if (typeof subsubsublist == "object") {
+							console.log(subsubsublist);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+ */
+// loop version
+function printLinkList(list) {
+	let temp = list;
+	while (temp) {
+		console.log(temp);
+		temp = temp.next;
+	}
+}
+
+// recursion version
+function printLinkListRecursion(list) {
+	if (list.next == null) {
+		console.log(list);
+	} else {
+		console.log(list);
+		printLinkListRecursion(list.next);
+	}
+}
+/* printLinkList(list); */
+printLinkListRecursion(list);
