c276321420ec7e7df567caad7987cbc604a97008
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-05 21:44:19 +0500

    more rubost version

--- JavaScript/recursion/containsAValue.js
+++ JavaScript/recursion/containsAValue.js
@@ -24,3 +24,16 @@ function contains(obj, value) {
 	return false;
 }
 console.log(contains(nestedObject, 44));
+
+
+function contains(object, searchValue) {
+  if (typeof object !== "object" || object === null) {
+    return object === searchValue;
+  }
+  for (const value of Object.values(object)) {
+    if (contains(value, searchValue)) {
+      return true;
+    }
+  }
+  return false;
+}
