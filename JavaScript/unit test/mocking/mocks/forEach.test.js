65469eb62704baf4b07d3a3348a15caee29ae1f6
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-25 20:58:23 +0500

    Add mock function of jest

---
+++ JavaScript/unit test/mocking/mocks/forEach.test.js
@@ -0,0 +1,16 @@
+const forEach = require("./forEach");
+
+let mockCallback = jest.fn((x) => 42 + x);
+
+test("forEach mock function", () => {
+	forEach([0, 1], mockCallback);
+
+	expect(mockCallback.mock.calls).toHaveLength(2);
+
+	expect(mockCallback.mock.results[0].value).toBe(42);
+
+  expect(mockCallback.mock.calls[1][0]).toBe(1);
+
+  expect(mockCallback.mock.calls[0][0]).toBe(0);
+
+});
