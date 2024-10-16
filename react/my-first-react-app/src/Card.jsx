bda13a2f6ed919eb145ebb74cf60ee1a3d7d9d06
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-16 09:46:26 +0500

    Create Card comp

---
+++ react/my-first-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+export function Card() {
+	return (
+		<div className="card">
+			<h2>This is a cool card</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+				quibusdam corrupti asperiores laudantium aliquid ipsum, expedita enim
+				repellat, similique at quis neque. Reiciendis quo error animi
+				dignissimos eum ipsa laudantium? Voluptate cupiditate molestias natus
+				blanditiis id. Magni culpa, sequi similique unde corrupti in libero modi
+				fuga, deserunt, blanditiis eligendi officiis!
+			</p>
+		</div>
+	);
+}
