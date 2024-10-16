1ab432c43480ed47ca030007a19ffa423bb3f9b9
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-16 10:13:47 +0500

    Use JSX rules to create a compnent

--- react/my-first-react-app/src/main.jsx
+++ react/my-first-react-app/src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+/* import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
-import { Card } from "./Card.jsx";
+import { Card } from "./Card.jsx"; */
+
+import { Rules } from "./Rules";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Greeting />
-		<Card name="No 01"/>
-    <Card name="No 02"/>
-    <Card name="No 03"/>
-    <Card name="No 04"/>
+		<Rules />
 	</StrictMode>
 );
