528a57da64de0864b74407b0e55f84cfc3959ff3
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-16 09:46:48 +0500

    Use compoent

--- react/my-first-react-app/src/main.jsx
+++ react/my-first-react-app/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Greeting from "./Greeting.jsx";
+import { Card } from "./Card.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<Greeting />
+		<Card />
+    <Card />
+    <Card />
+    <Card />
+	</StrictMode>
+);
