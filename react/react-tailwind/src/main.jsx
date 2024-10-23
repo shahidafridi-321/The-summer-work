5749def92e07e768d6e1fe878fb5daf775d08b1a
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-23 17:18:11 +0500

    Create FeedbackForm comp

--- react/react-tailwind/src/main.jsx
+++ react/react-tailwind/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Form from "./Form";
+/* import Form from "./Form"; */
 /* import { Gallery } from "./ImageCrosal"; */
 /* import  Counter  from "./Counter"; */
 /* import { App } from "./App"; */
 /* import App from "./BackgroundColorChange"; */
 import "./index.css";
+import FeedbackForm from "./FeedbackForm";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Form />
+		<FeedbackForm />
 	</StrictMode>
 );
