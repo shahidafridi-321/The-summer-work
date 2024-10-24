dec3bef2ed6a5b7c43adccd595bafd7440880b65
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-24 10:37:34 +0500

    Add trafic component

---
+++ react/react-tailwind/src/Trafic.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function TrafficLight() {
+	const [walk, setWalk] = useState(true);
+
+	function handleClick() {
+		setWalk(!walk);
+		if (walk) {
+			alert("Stop is next");
+		} else {
+			alert("Walk is next");
+		}
+	}
+
+	return (
+		<>
+			<button onClick={handleClick}>Change to {walk ? "Stop" : "Walk"}</button>
+			<h1
+				style={{
+					color: walk ? "darkgreen" : "darkred",
+				}}
+			>
+				{walk ? "Walk" : "Stop"}
+			</h1>
+		</>
+	);
+}
