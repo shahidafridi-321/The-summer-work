9b3b99a7cc712e155fd8e4a2433428cfa34e934d
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-30 15:50:17 +0500

    use Room component

--- react/rendring-in-react/src/main.jsx
+++ react/rendring-in-react/src/main.jsx
@@ -1,20 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Challenge2 } from "../Challenge2";
-import { DrinkList } from "../Challenge3";
-import { EveryOneElse, Chemists } from "../Chemists";
-import Poem from "./Poem";
 
-import { RecipeList } from "./Recipes";
-
-import { RendringListsOfData } from "./RendringListsOfData";
-
-import { Animals } from "./AnimalsList";
-import { ItemsList } from "./ConditionallyReturningJSX";
-import "./index.css";
+import { Room } from "./UseEffectWorking";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Poem />
+		<Room />
 	</StrictMode>
 );
