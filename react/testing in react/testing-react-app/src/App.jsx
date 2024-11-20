7aff06800cbd19946d55a2be7368bc2232917ce0
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-11-20 16:20:17 +0500

    First test

--- react/testing in react/testing-react-app/src/App.jsx
+++ react/testing in react/testing-react-app/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+// App.jsx
+
+import { useState } from "react";
+
+const App = () => {
+	const [heading, setHeading] = useState("Magnificent Monkeys");
+
+	const handleClick = () => {
+		setHeading("Radical Rhinos");
+	};
+
 	return (
 		<>
-			<h1>heool</h1>
+			<button onClick={handleClick}>Click Me</button>
+			<h1>{heading}</h1>
 		</>
 	);
-}
+};
 
 export default App;
