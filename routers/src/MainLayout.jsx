4e84df0a15bffa6e07bf938cfedb6709d65c4b3a
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-11-28 17:57:16 +0500

    Create mainlayout

--- routers/src/MainLayout.jsx
+++ routers/src/MainLayout.jsx
@@ -1,20 +1,19 @@
-import React from "react";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
+import { Outlet } from "react-router-dom";
 
 const styles = {
-  height:"100vh",
+	height: "100vh",
 	display: "flex",
 	flexDirection: "column",
 	justifyContent: "space-between",
-
-
 };
 
 export const MainLayout = () => {
 	return (
 		<div style={styles}>
 			<Navbar />
+			<Outlet />
 			<Footer />
 		</div>
 	);
