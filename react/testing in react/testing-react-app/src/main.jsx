5a766cdce6ad9300789f425cfe7ca1641ef13ba3
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-11-28 10:54:56 +0500

    Create Root page

--- react/testing in react/testing-react-app/src/main.jsx
+++ react/testing in react/testing-react-app/src/main.jsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./contact-app/index.css";
 
+import Root from "./contact-app/routes/routes";
+
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
+	{
+		path: "/",
+		element: <Root />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
