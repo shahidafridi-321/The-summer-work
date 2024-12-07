968d778cf1b88a7a4b74fc59c4fa644c12c52f19
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-07 19:41:48 +0500

    Create a separate module that provides services for notes updating, creating and getting all the notes to display

---
+++ routers/src/services.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3002/notes";
+
+const getAll = () => {
+	return axios.get(baseURL);
+};
+
+const create = (newObject) => {
+	return axios.post(baseURL, newObject);
+};
+
+const update = (id, newObject) => {
+	return axios.put(`${baseURL}/${id}`, newObject);
+};
+
+export default {
+	getAll,
+	create,
+	update,
+};
