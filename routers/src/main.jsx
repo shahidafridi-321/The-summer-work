dd6b7b358d118c5a0b605fb90137fcf7c3612062
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-08 15:55:56 +0500

    render image component

--- routers/src/main.jsx
+++ routers/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Display } from "./Display";
 import Phonebook from "./PhoneBook";
+import { Image } from "./Image";
+import Profile from "./Profle";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Phonebook />
+		<Profile  delay={1000}/>
 	</StrictMode>
 );
