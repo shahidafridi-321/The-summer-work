848a136d43ee85adddbe57f9e854d9fd13667d80
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-12 11:37:23 +0500

    Display posts component

--- routers/src/main.jsx
+++ routers/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Display } from "./Display";
 import Phonebook from "./PhoneBook";
-import { Image } from "./Image";
 import Profile from "./Profle";
+import {Image} from "./FetchingData/Image";
+import { Posts } from "./FetchingData/Posts";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Phonebook />
+		<Posts />
 	</StrictMode>
 );
