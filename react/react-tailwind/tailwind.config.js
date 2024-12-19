60fc86e9889062030e4403bd637753ab6e97de3c
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-12-19 19:25:18 +0500

    Add custom colors configuration

--- react/react-tailwind/tailwind.config.js
+++ react/react-tailwind/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
 	theme: {
 		extend: {
+			colors: {
+				darkBg: "#111827",
+				getStartedBtn: "#0f766e",
+			},
 			typography: {
 				DEFAULT: {
 					css: {
